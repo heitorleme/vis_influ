dff6e182c20e9ac9c21e1c50d27a85621c75c536
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-05-19 21:46:21 +0100

    Update infos_influencers.py

--- infos_influencers.py
+++ infos_influencers.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import json
+import io
+from datetime import datetime
 
 # Upload de múltiplos arquivos JSON
 uploaded_files = st.file_uploader("Carregue os arquivos JSON dos influencers", type="json", accept_multiple_files=True)
-
 
 # Inicialização
 influencers = []
@@ -40,26 +41,28 @@ if uploaded_files:
 
     if not df_cidades.empty:
         df_cidades.rename(columns={"name": "Cidade"}, inplace=True)
+        df_cidades_exibicao = df_cidades.copy()
+        df_cidades_exibicao.drop(columns=["coords.lat", "coords.lon", "country.id", "country.code"], inplace=True)
+        df_cidades_exibicao.rename(columns={"weight":"Porcentagem da audiência"}, inplace=True)
+        df_cidades_exibicao = df_cidades_exibicao.sort_values(by=["influencer", "weight"], ascending=[True, False]).groupby("influencer").head(top_n)
 
         # Mostrar tabela original
         st.subheader("Cidades por Influencer")
-        st.dataframe(df_cidades)
+        st.dataframe(df_cidades_exibicao)
 
-        # Ordenar e selecionar as N cidades com maiores pesos por influencer
-        result = df_cidades.sort_values(by=["influencer", "weight"], ascending=[True, False])\
-                           .groupby("influencer")\
-                           .head(top_n)
+        # Botão para exportar a tabela para Excel
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df_cidades_exibicao.to_excel(writer, index=False, sheet_name='Cidades')
+            writer.save()
+            processed_data = output.getvalue()
 
-        # Reformatar para tabela pivot
-        result["rank"] = result.groupby("influencer").cumcount() + 1
-        pivot_df = result.pivot(index="influencer", columns="rank", values=["Cidade", "weight"])
-        pivot_df.columns = [f"{col[0]} #{col[1]}" for col in pivot_df.columns]
-        pivot_df.reset_index(inplace=True)
-
-        # Mostrar tabela reformulada
-        st.subheader(f"Top {top_n} Cidades por Influencer (ordenadas por peso)")
-        st.dataframe(pivot_df)
-    else:
-        st.info("Nenhum dado de cidades encontrado nos arquivos.")
+        st.download_button(
+            label="📥 Baixar tabela de cidades como Excel",
+            data=processed_data,
+            data_hoje = datetime.today().strftime("%Y-%m-%d")
+            file_name = f"cidades_por_influencer_{data_hoje}.xlsx"
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
 else:
     st.info("Por favor, carregue arquivos JSON para começar.")
