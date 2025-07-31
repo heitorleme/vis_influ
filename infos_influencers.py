09fc4052c047aacaf980094cd64aba2f64a0d93c
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-07-31 17:53:46 +0100

    Update infos_influencers.py

--- infos_influencers.py
+++ infos_influencers.py
@@ -405,9 +405,6 @@ with abas[1]:
 		df_resumo = pd.DataFrame(resumo_influenciadores)
 		st.session_state["df_resumo"] = df_resumo
 
-		df_resumo_formatado = df_resumo.style.set_properties(**{
-		    'white-space': 'pre-wrap'
-		})
-		st.dataframe(df_resumo_formatado)
+		st.table(df_resumo)
 	else:
 		st.warning("Por favor, faça o upload de arquivos JSON válidos na primeira aba")
