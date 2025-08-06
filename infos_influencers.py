0051e49b41716bd198d81d74a65900a8f20f4be4
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-08-06 17:09:53 +0100

    Update infos_influencers.py

--- infos_influencers.py
+++ infos_influencers.py
@@ -545,9 +545,10 @@ with abas[2]:
 		if influenciador_selecionado:
 			exibir_posts(
 				influencer=influenciador_selecionado,
-				dados=st.session_state["dados_brutos"]
+				dados_brutos=st.session_state["dados_brutos"]
 			)
 	
 	else:
 		st.warning("Por favor, faça o upload de arquivos JSON válidos na primeira aba")
+
 
